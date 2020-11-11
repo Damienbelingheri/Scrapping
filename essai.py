752a8a64d2d92a8f1579aa30edecf466abbf2bed
damienBelingheri <belingheridamien@gmail.com>
2020-11-11 02:54:47 +0100

    problem overwriting .scv solved

--- essai.py
+++ essai.py
@@ -11,7 +11,8 @@ import re
 
 # specify the url
 urlpage = 'https://www.sous-traiter.com/annuaire/societe-ryg-95100-argenteuil-2888.html'
-urlpage = 'https://www.sous-traiter.com/annuaire/fichequart.php?codeAnnuaire=11162'
+#urlpage = 'https://www.sous-traiter.com/annuaire/fichequart.php?codeAnnuaire=11162'
+
 # query the website and return the html to the variable 'page'
 page = requests.get(urlpage)
 # parse the html using beautiful soup and store in variable 'soup'
@@ -20,20 +21,15 @@ soup = BeautifulSoup(page.text, 'html.parser')
 table = soup.find('section',{'id': 'section-infos'})
 
 # search if the company is premium reaching content
-title = soup.find_all('i')
-print (title[4].getText())
+occurence = soup.find_all('i')
+premium = occurence[4].getText()
 
+rows = []
+rows.append(['Company Name','CodePostal', 'Location', 'Phone','Email','Website','UrlPage'])
 data = table.find_all('span')
 a = table.find_all('a',href=True)
 
-#print('Number of results', len(results))
-
-# create and write headers to a list 
-rows = []
-rows.append(['Company Name','CodePostal', 'Location', 'Phone','Email','Website','UrlPage'])
-
-if(title[4].getText() == 'Premium'):
-    #print('nique ta mère')
+if(premium == 'Premium'):
     company =  data[0].getText()
     location = data[1].getText()
     phone = a[0]['href'].strip('tel:').replace(" ", " ") 
@@ -41,29 +37,28 @@ if(title[4].getText() == 'Premium'):
     website = None
     codePostal = re.findall(r'\d+', location)[-1]
 
-
-    
-    #print (company,location,phone,website)
-
 else:
-
-
     company =  data[0].getText()
     location = data[2].getText() +" "+ data[3].getText() + " " + data[4].getText()
     phone = a[0]['href'].strip('tel:').replace(" ", " ")
-    #fax = data[6].getText()
-    email = a[2].getText() 
+    # fax = data[6].getText()
+    email = a[2].getText()
     codePostal= data[3].getText()
-    #print(website)
+    try:
+        website= a[3]['href']
+    except:
+        website = 'none'
 
 
-# loop over results
 
-rows.append([company,codePostal,location, phone,email, website])
-#print(rows)
 
-    
-## Create csv and write rows to output file
+    # loop over results
+
+    rows.append([company,codePostal,location, phone,email, website])
+    #print(rows)
+
+        
+    ## Create csv and write rows to output file
 with open('techtrack100.csv','w', newline='') as f_output:
     csv_output = csv.writer(f_output)
     csv_output.writerows(rows)
--- scrapping.py
+++ scrapping.py
@@ -8,6 +8,8 @@ import time
 import csv
 import re
 from fake_useragent import UserAgent
+from urllib.request import urlopen
+
 
 token = 'https://www.sous-traiter.com/annuaire/liste.php?metier=Usinage,%20M%C3%A9canique&page='
 
@@ -24,31 +26,35 @@ pages = get_pages(token,1)
 proxies = pd.read_csv('proxy_list.txt', header = None)
 proxies = proxies.values.tolist()
 proxies = list(it.chain.from_iterable(proxies))
-proxy_pool = it.cycle(proxies)
-proxy = next(proxy_pool)
+
 
 def get_data(pages,proxies):
 
-    df = pd.DataFrame()
+  
     parameters = ['']
     ua = UserAgent()
     proxy_pool = it.cycle(proxies)
 
     rows = []
     rows.append(['Company Name', 'Webpage', 'Description', 'Location','Email','Phone'])
-    while len(pages) > 0:
+    #while len(pages) > 0:
 
-    #for i in range(0, 1, 1):
+    for i in range(0, 2, 1):
         for i in pages:
+            #get the url of the page
             response = requests.get(i)
+
 
             soup = bs4.BeautifulSoup(response.text, 'html.parser')
             em_box = soup.find_all("a", {"class":"button-reveal"})
             rows = []
             rows.append(['Company Name','CodePostal', 'Location', 'Phone','Email','Website','UrlPage'])
             for result in em_box:
-                result = result['href']
 
+                proxy = next(proxy_pool)
+                result = result['href']
+                
+                
                 urlpage = 'https://www.sous-traiter.com/annuaire/'+ result
                 
                 # query the website and return the html to the variable 'page'
@@ -78,7 +84,6 @@ def get_data(pages,proxies):
                     company =  data[0].getText()
                     location = data[2].getText() +" "+ data[3].getText() + " " + data[4].getText()
                     phone = a[0]['href'].strip('tel:').replace(" ", " ")
-                    print(phone)
                     # fax = data[6].getText()
                     email = a[2].getText()
                     codePostal= data[3].getText()
@@ -92,11 +97,11 @@ def get_data(pages,proxies):
 
 
                 rows.append([company,codePostal,location, phone,email, website,urlpage])
-
-            print(rows)
-            with open('scrapping.csv','w', newline='') as f_output:
-                csv_output = csv.writer(f_output)
-                csv_output.writerows(rows)
+                print(rows)
+           
+    with open('usinage-mecanique.csv','a', newline='') as f_output:
+        csv_output = csv.writer(f_output)
+        csv_output.writerows(rows)
+            
     
-
 data = get_data(pages,proxies)
